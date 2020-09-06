01ba8690d614a71dcd89091a95f1eeb6d2f12591
Leandro Paz <leandropaz@gmail.com>
2020-09-06 15:13:25 -0300

    (fix): remove / from cors url...

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'https://leandropaz-app-grades.herokuapp.com/',
+    origin: 'https://leandropaz-app-grades.herokuapp.com',
   })
 );
 app.use('/', gradeRouter);
