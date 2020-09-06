768317215700874bd3687eb5e1890029c6e4f77b
Leandro Paz <leandropaz@gmail.com>
2020-09-06 15:08:38 -0300

    (fix): origin URL for cors policy

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'http://localhost:8080',
+    origin: 'https://leandropaz-app-grades.herokuapp.com/',
   })
 );
 app.use('/', gradeRouter);
